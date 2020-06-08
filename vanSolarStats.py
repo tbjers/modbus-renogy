a8c3962bc7c40860c9d17e68d3d34769a8618091
PHo <pho@apoapse.co.uk>
2020-06-08 10:18:37 +0100

    latest

---
+++ vanSolarStats.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+import minimalmodbus
+import json
+import paho.mqtt.client as mqtt
+from datetime import datetime
+
+instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)  # port name, slave address (in decimal)
+
+instrument.serial.baudrate = 9600
+instrument.serial.bytesize = 8
+instrument.serial.parity = minimalmodbus.serial.PARITY_NONE
+instrument.serial.stopbits = 2
+instrument.serial.timeout  = 1
+
+instrument.address = 1  # this is the slave address number
+instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
+instrument.clear_buffers_before_each_transaction = True
+
+# print(instrument)   # Print RS485 connection config
+
+broker_url = '192.168.69.106'
+broker_port = 1883
+
+mqttPublishPayload = json.dumps({
+	"auxSoc": instrument.read_register(256),
+	"auxVoltage": instrument.read_register(257,1),
+	"maxCharge": instrument.read_register(258,2),
+	"controllerTemp": instrument.read_register(259),
+	"auxTemp": instrument.read_register(259),
+	"altVoltage": instrument.read_register(260,1),
+	"altAmps": instrument.read_register(261,2),
+	"altWatts": instrument.read_register(262),
+	"solVoltage": instrument.read_register(263,1),
+	"solAmps": instrument.read_register(264,2),
+	"solWatts": instrument.read_register(265),
+	"dayCount": instrument.read_register(271),
+	"chargeState": instrument.read_register(276),
+	"faultBits1": instrument.read_register(277),
+	"faultBits2": instrument.read_register(278),
+	"timestamp": str(datetime.now())
+})
+
+#mqttPublishPayload = json.dumps(mqttPublishPayload)
+
+try:
+	client = mqtt.Client()
+	client.connect(broker_url, broker_port)
+	client.publish(
+		topic="van/solar",
+		payload=mqttPublishPayload,
+		retain=True
+	)
+except:
+	print ("Error")
+
