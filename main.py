0e8a802f0b8296fdd672bfbcd495e537ffa928a3
PHo <pho@apoapse.co.uk>
2020-10-23 01:11:51 +0100

    added charge state and error code registers

--- main.py
+++ main.py
@@ -15,6 +15,35 @@ instrument.address = 1  # this is the slave address number
 instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
 instrument.clear_buffers_before_each_transaction = True
 
+# Charge State
+chargeState = instrument.read_register(256)
+chargingNone = (chargeState & 0b0000000000000000) > 0
+chargingSolar = (chargeState & 0b0000000000000100) > 0
+chargingEqualization = (chargeState & 0b0000000000001000) > 0
+chargingBoost = (chargeState & 0b0000000000010000) > 0
+chargingFloat = (chargeState & 0b0000000000100000) > 0
+chargingLimited = (chargeState & 0b0000000001000000) > 0
+chargingAlt = (chargeState & 0b0000000010000000) > 0
+
+# Error Codes
+errorCodesLow = instrument.read_register(257)
+errorCtrlOverTemp2 = ( errorCodesLow & 0b0000000000010000) > 0
+errorAltInputOverCurrent = (errorCodesLow & 0b0000000000100000) > 0
+errorAltInputOverVolt = (errorCodesLow & 0b0000000100000000) > 0
+errorAltPolarityReverse = (errorCodesLow & 0b0000001000000000) > 0
+errorBmsOverCharge = (errorCodesLow & 0b0000010000000000) > 0
+errorAuxLowTemp = (errorCodesLow & 0b0000100000000000) > 0
+
+errorCodesHigh = instrument.read_register(258)
+errorAuxOverDischarge = ( errorCodesHigh & 0b0000000000000001) > 0
+errorAuxOverVolt = ( errorCodesHigh & 0b0000000000000010) > 0
+errorAuxUnderVolt = ( errorCodesHigh & 0b0000000000000100) > 0
+errorCtrlOverTemp = ( errorCodesHigh & 0b0000000000100000) > 0
+errorAuxOverTemp = ( errorCodesHigh & 0b0000000001000000) > 0
+errorSolInputTooHigh = ( errorCodesHigh & 0b0000000010000000) > 0
+errorSolInputOverVolt = ( errorCodesHigh & 0b0000001000000000) > 0
+errorSolReversePolarity = ( errorCodesHigh & 0b0001000000000000) > 0
+
 mqttPublishPayload = json.dumps({
 	"auxSoc": instrument.read_register(256),
 	"auxVoltage": instrument.read_register(257,1),
@@ -31,7 +60,29 @@ mqttPublishPayload = json.dumps({
 	"chargeState": instrument.read_register(276),
 	"faultBits1": instrument.read_register(277),
 	"faultBits2": instrument.read_register(278),
-	"timestamp": str(datetime.now())
+	"timestamp": str(datetime.now()),
+	"chargingNone": chargingNone,
+	"chargingSolar": chargingSolar,
+	"chargingEqualization": chargingEqualization,
+	"chargingBoost": chargingBoost,
+	"chargingFloat": chargingFloat,
+	"chargingLimited": chargingLimited,
+	"chargingAlt": chargingAlt
+	"errorCtrlOverTemp2": errorCtrlOverTemp2,
+	"errorAltInputOverCurrent": errorAltInputOverCurrent,
+	"errorAltInputOverVolt": errorAltInputOverVolt,
+	"errorAltPolarityReverse ": errorAltPolarityReverse,
+	"errorBmsOverCharge": errorBmsOverCharge,
+	"errorAuxLowTemp": errorAuxLowTemp,
+	"errorCodesHigh": errorCodesHigh,
+	"errorAuxOverDischarge": errorAuxOverDischarge,
+	"errorAuxOverVolt": errorAuxOverVolt,
+	"errorAuxUnderVolt": errorAuxUnderVolt,
+	"errorCtrlOverTemp": errorCtrlOverTemp,
+	"errorAuxOverTemp": errorAuxOverTemp,
+	"errorSolInputTooHigh": errorSolInputTooHigh,
+	"errorSolInputOverVolt": errorSolInputOverVolt,
+	"errorSolReversePolarity": errorSolReversePolarity,
 })
 
 try:
